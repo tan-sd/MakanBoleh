dbe5c878a4a496b303fa5c00cdb4166e178e5de6
faezlatiff <muhammadal.2021@scis.smu.edu.sg>
2023-03-10 13:50:48 +0800

    added database and added food_info.py

--- food_info/food_db.sql
+++ food_info/food_db.sql
@@ -1,76 +1,31 @@
 -- this is the database setup file
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 --
--- Database: `order`
+-- Database: `food_db`
 --
 CREATE DATABASE IF NOT EXISTS `food_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `food_db`;
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `food_db`
---
+-- Table structure for table `food_table` in database `food_db`
 
-DROP TABLE IF EXISTS `food_db`;
-CREATE TABLE IF NOT EXISTS `food_db` (
+DROP TABLE IF EXISTS `food_table`;
+CREATE TABLE IF NOT EXISTS `food_info` (
   
-  `post_id` int(10) -- post_id is randomly generated 
+  `post_id` int(11) NOT NULL AUTO_INCREMENT, -- post_id is randomly generated 
   `creator_id` int(11) NOT NULL AUTO_INCREMENT,
-  `customer_id` varchar(32) NOT NULL,
-  `status` varchar(10) NOT NULL DEFAULT 'NEW',
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`order_id`)
+  `time_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_name` varchar(60),
+  `latitude` float(15),
+  `longitude` float(15),
+  `description` varchar(1000) NULL,
+  `allergens` varchar(8000) NULL, 
+  'is_available' bit
+
+  PRIMARY KEY (`post_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `order`
---
-
-INSERT INTO `order` (`order_id`, `customer_id`, `status`, `created`, `modified`) VALUES
-(1, 'Apple TAN', 'NEW', '2020-06-12 02:14:55', '2020-06-12 02:14:55');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `order_item`
---
-
-DROP TABLE IF EXISTS `order_item`;
-CREATE TABLE IF NOT EXISTS `order_item` (
-  `item_id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_id` int(11) NOT NULL,
-  `book_id` char(13) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  PRIMARY KEY (`item_id`),
-  KEY `FK_order_id` (`order_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `order_item`
---
-
-INSERT INTO `order_item` (`item_id`, `order_id`, `book_id`, `quantity`) VALUES
-(1, 1, '9781434474234', 1),
-(2, 1, '9781449474212', 1);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `order_item`
---
-ALTER TABLE `order_item`
-  ADD CONSTRAINT `FK_order_id` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
