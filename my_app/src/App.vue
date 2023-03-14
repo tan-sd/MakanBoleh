84c5fdff30b5a3a4d9d44051485f51fdbfb87d84
Sheng <shengdatan@gmail.com>
2023-03-14 09:11:20 +0800

    Route the buttons to respective views/components.

--- my_app/src/App.vue
+++ my_app/src/App.vue
@@ -1,39 +1,21 @@
 <template>
-  <!-- <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/user">User Profile</router-link>
-  </div> -->
-  <!-- <router-view/> -->
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <!-- <button class="btn btn-main-light">Hi There</button> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-  <img
-      src="./assets/images/logos/small_logos/MakanBoleh_logo_long_light_small.png"
-      style="margin-bottom: 10px; width: 25vw;"
-    />
-  <Map/>
+  <NavBar/>
+  <!-- <Map/> -->
+  <!-- <FoodList/> -->
+  <router-view></router-view>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
-import Map from "./components/Map.vue";
+// import Map from "./components/Map.vue";
+import NavBar from "./components/NavBar.vue";
+// import FoodList from "./components/FoodList.vue";
 
 export default {
   name: "App",
   components: {
-    // HelloWorld,
-    Map
+    // Map,
+    NavBar,
+    // FoodList
   },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- my_app/src/views/Home.vue
+++ my_app/src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>
-        Home
-    </div>
-</template>
+    <Map/>
+    <FoodList/>
+  </template>
+  
+  <script>
+  import Map from "../components/Map.vue";
+  import FoodList from "../components/FoodList.vue";
+  
+  export default {
+    name: "App",
+    components: {
+      Map,
+      FoodList
+    },
+  };
+  </script>
