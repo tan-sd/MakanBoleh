cb0f4fbdbbc529fcb6f1dfe86a61290fcaeecd83
rrachea <rrachelsng@gmail.com>
2023-03-14 15:41:43 +0800

    userdb updated w email etc

--- microservices/user_info/user_info.sql
+++ microservices/user_info/user_info.sql
@@ -5,6 +5,9 @@ DROP TABLE IF EXISTS `user_info`;
 CREATE TABLE IF NOT EXISTS `user_info` (
   `user_id` int(11) NOT NULL auto_increment,
   `name` varchar(64) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
   `address` varchar(64) NOT NULL,
   `latitude` decimal(10,6) ,
   `longitude` decimal(10,6) ,
@@ -17,10 +20,10 @@ CREATE TABLE IF NOT EXISTS `user_info` (
 -- Dumping data for table `user_info`
 --
 
-INSERT INTO `user_info` (`name`, `address`, `latitude`,`longitude`,`dietary_type`, `travel_appetite`) 
+INSERT INTO `user_info` (`name`, `username`, `email`, `password`, `address`, `latitude`,`longitude`,`dietary_type`, `travel_appetite`) 
 VALUES
-('ShengJingBing', 'Singapore Marina Bay Sands', 1.283375, 103.860725 , 'na', 'far'),
-('DancerAdam', 'Singapore Management University', 41.023472, -91.967133 , 'na', 'far');
+('ShengJingBing', 'SJB123', 'sjb@gmail.com', 'Password12345!', 'Singapore Marina Bay Sands', 1.283375, 103.860725 , 'na', 'far'),
+('DancerAdam', 'DA123', 'dancer@gmail.com', 'Password12345!', 'Singapore Management University', 41.023472, -91.967133 , 'na', 'far');
 COMMIT;
 
 SELECT * FROM user_info;
